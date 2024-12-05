fed2eef6441cb5389e8a65c848c73562b5891eee
ORACLEO-SVKJCNH\Ashish Rai <ashish.a.rai@oracle.com>
2024-12-05 13:30:18 +0530

    Singleton

---
+++ com.ashishrai.design_patterns/src/main/java/com/ashishrai/design_patterns/creational/singleton/EnumSingleton.java
@@ -0,0 +1,12 @@
+
+package com.ashishrai.design_patterns.creational.singleton;
+
+public enum EnumSingleton {
+
+	INSTANCE;
+
+	public void doSomething() {
+
+		System.out.println("Working as an Enum Singleton!");
+	}
+}
